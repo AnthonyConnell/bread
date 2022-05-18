4f85872d5160f55d3fc2697275c505e0e6922713
Anthony Connell <toco85@gmail.com>
2022-05-18 19:12:03 -0400

    Helper Methods

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const Bread = require('../models/bread.js')
 const bread = require('../models/bread.js')
 const breads = express.Router()
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: bread,
-        title: 'Index Page'
-      }
-    )
-  // res.send(Bread)
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
+
 
 // NEW
 breads.get('/new', (req, res) => {
@@ -19,54 +21,60 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread: bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    })
-  } else {
-    res.send('404')
-  }
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
+      })
 })
 
+
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id)
+    .then(deletedBread => {
+      res.status(303).redirect('/breads')
+    })
 })
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('edit', {
+        bread: foundBread
+      })
+    })
 })
 
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    req.body.image = undefined
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  bread.push(req.body)
+  bread.create(req.body)
   res.redirect('/breads')
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(updatedBread => {
+      console.log(updatedBread)
+      res.redirect(`/breads/${req.params.id}`)
+    })
 })
 
 module.exports = breads
